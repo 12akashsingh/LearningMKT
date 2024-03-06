4d1dea2ddbb19a194f5becf26e19ae96a3657538
akash.singh <singh.1994akash@gmail.com>
2024-03-06 23:07:37 +0530

    06-03-2024 Assignment

---
+++ Flipkart_NameLocator.java
@@ -0,0 +1,19 @@
+package basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Flipkart_NameLocator {
+
+	public static void main(String[] args) 
+	{
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		
+		driver.findElement(By.name("q")).sendKeys("shoes"+Keys.ENTER);
+		
+				
+	}
+
+}
